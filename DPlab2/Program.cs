0e010b724c54fa4015015c5d9ed2fc6bc7905ddf
MrVictorPlus <164523783+MrVictorPlus@users.noreply.github.com>
2025-11-30 14:18:37 +0200

    Add files via upload

---
+++ DPlab2/Program.cs
@@ -0,0 +1,34 @@
+﻿//Console.WriteLine("Hello, World!");
+//Console.ReadKey();
+
+Console.WriteLine("Hello, Jerry!");
+
+for (int i = 0; i < 3; i++)
+{
+    Console.WriteLine("A");
+    Console.WriteLine("B");
+    Console.WriteLine("C");
+    Thread.Sleep(500);
+}
+
+A();
+A();
+A();
+
+void A()
+{
+    Console.WriteLine("Функция A");
+    B();
+    C();
+}
+
+void B()
+{
+    Console.WriteLine("Функция B");
+}
+
+void C()
+{
+    Console.WriteLine("Функция C");
+}
+
---
+++ DPlab3/Program.cs
@@ -0,0 +1,47 @@
+﻿struct Prices
+{
+    public int Drink;
+    public int FirstDish;
+    public int SecondDish;
+}
+
+struct Order
+{
+    public int DrinkCount;
+    public int FirstDishCount;
+    public int SecondDishCount;
+}
+
+class Program
+{
+    static int CalculateOrderCost(Prices prices, Order order)
+    {
+        return prices.Drink * order.DrinkCount +
+               prices.FirstDish * order.FirstDishCount +
+               prices.SecondDish * order.SecondDishCount;
+    }
+
+    static void Main()
+    {
+        Prices menuPrices;
+        menuPrices.Drink = 10;
+        menuPrices.FirstDish = 20;
+        menuPrices.SecondDish = 30;
+
+        Order client1Order;
+        client1Order.DrinkCount = 2;
+        client1Order.FirstDishCount = 1;
+        client1Order.SecondDishCount = 0;
+
+        int client1Total = CalculateOrderCost(menuPrices, client1Order);
+        System.Console.WriteLine("Стоимость заказа первого клиента: " + client1Total);
+
+        Order client2Order;
+        client2Order.DrinkCount = 1;
+        client2Order.FirstDishCount = 2;
+        client2Order.SecondDishCount = 3;
+
+        int client2Total = CalculateOrderCost(menuPrices, client2Order);
+        System.Console.WriteLine("Стоимость заказа второго клиента: " + client2Total);
+    }
+}
